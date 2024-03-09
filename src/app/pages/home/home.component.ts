95c1992f8738a0edfcf92869b801844a668c94cc
Gustavo Reimche <gustavo.reimche@linx.com.br>
2024-03-09 20:52:36 -0300

    ajustes e bckp

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,6 +9,8 @@ import { ScheduleService } from '../../services/schedule.service';
 import { GetHours } from '../../utils/utils-hours';
 import { DateService } from '../../services/date.service';
 import { take, timer } from 'rxjs';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { bck } from '../../utils/bck';
 
 @Component({
   selector: 'app-home',
@@ -18,6 +20,7 @@ import { take, timer } from 'rxjs';
     ToastModule,
     LoadingComponent,
     ScheduleTableComponent,
+    HttpClientModule,
   ],
   templateUrl: './home.component.html',
   styles: ``,
@@ -32,23 +35,53 @@ export class HomeComponent {
   constructor(
     private _service: ScheduleService,
     private _loading: LoadingService,
-    private _dateService: DateService
+    private _dateService: DateService,
+    private http: HttpClient
   ) {}
 
   ngOnInit(): void {
     this._loading.start();
     this.hours = GetHours();
     this.mountRenderList();
-    this._service.filterByDate(this.date).pipe(take(1)).subscribe((schedules) => {
-      this.schedulesDate = schedules;
-      this.mountRenderList();
-      this._loading.stop();
-    });
+    this._service
+      .filterByDate(this.date)
+      .pipe(take(1))
+      .subscribe((schedules) => {
+        this.schedulesDate = schedules;
+        this.mountRenderList();
+        this._loading.stop();
+      });
     this.getSchedulesByDate();
     this._dateService.getDate().subscribe((date) => {
       this.date = date;
       this.getSchedulesByDate();
-    })
+    });
+    // bck.forEach((item) => {
+    //   const year = item.year || 0; // Defina um valor padrão se year for nulo ou indefinido
+    //   const month = item.month || 1; // Defina um valor padrão se month for nulo ou indefinido
+    //   const day = item.day || 1; // Defina um valor padrão se day for nulo ou indefinido
+
+    //   // Verifique se os valores de ano, mês e dia são válidos
+    //   if (year >= 2024 && month >= 0 && month <= 12 && day >= 1 && day <= 31) {
+    //     const date = new Date(
+    //       `${year}-${(month+1).toString().padStart(2, '0')}-${day
+    //         .toString()
+    //         .padStart(2, '0')}T12:00:00Z`
+    //     );
+    //     const payload = {
+    //       date: date.toISOString(),
+    //       hour: item.hora,
+    //       client: item.nome,
+    //       serviceExecuted: item.servico,
+    //       value: item.valor,
+    //     };
+    //     console.log('=> ', payload.date);
+    //     this._service.create(payload).subscribe();
+    //   } else {
+    //     console.error('Invalid date values:', item);
+    //   }
+    // });
+    // console.log('END');
   }
 
   getSchedulesByDate() {
